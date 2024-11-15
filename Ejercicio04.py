22ae5f2b90c1158c066deb2e18f8917d4fc24df7
dgomarasad <dgomarasad@educacion.navarra.es>
2024-11-15 09:36:50 +0100

    Create Ejercicio04.py

---
+++ Ejercicio04.py
@@ -0,0 +1,11 @@
+paises = input('Dime 3 paises que te gustaria conocer:')
+'''Entrada = paises que te gustaria conocer
+   Salida = Me gustaria visitar +variable'''
+print('Me gustaria visitar',paises[0:6])
+print('Me gustaria visitar',paises[7:13])
+print('Me gustaria visitar',paises[13:22])
+
+numeros = input('Dime numeros separados por comas:')
+len(numeros)
+print(min(numeros))
+print(max(numeros))
