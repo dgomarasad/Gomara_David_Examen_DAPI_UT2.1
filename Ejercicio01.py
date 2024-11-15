bb41a74208bb86d0c5bdfa8c7799fee6ac24387a
dgomarasad <dgomarasad@educacion.navarra.es>
2024-11-15 09:36:41 +0100

    Create Ejercicio01.py

---
+++ Ejercicio01.py
@@ -0,0 +1,9 @@
+'''Crear la variable comida '''
+comida = input('Cual es tu comida favorita: ')
+'''Imprimir la variable comida'''
+print('Me encanta',comida,'tambien!')
+'''entrada = edad
+   salida= entrada x 12 =meses vividos'''
+edad = input('Cual es tu edad en años: ')
+meses = (edad*12)
+print('Has vivido aproximadamente',meses,'meses')
