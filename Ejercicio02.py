4c374aa41e404eb27e36089e8697dbe344d08278
dgomarasad <dgomarasad@educacion.navarra.es>
2024-11-15 09:36:44 +0100

    Create Ejercicio02.py

---
+++ Ejercicio02.py
@@ -0,0 +1,7 @@
+Nombre_completo = input('Dime tu nombre y apellido:')
+'''Entrada = Nombre y apellido
+   Salida = Nombre'''
+print(Nombre_completo[:-7])
+Palabra = input('Dime una palabra:')
+for vocal in Palabra:
+    print((vocal.upper()))
