11e56ae6c5b1fdbb99e5eef0694ffd1af315bcdd
dgomarasad <dgomarasad@educacion.navarra.es>
2024-11-15 09:36:46 +0100

    Create Ejercicio03.py

---
+++ Ejercicio03.py
@@ -0,0 +1,8 @@
+numero = int(input('Dime un numero entero:'))
+'''Creamos condicional
+    entrada =numero entero
+    salida= el numero es par o impar''' 
+if numero %2:
+    print('El numero es par') 
+elif numero (-1):
+    print('El numero es impar' )
